e3686a8621f08191e38c61b6b5b6db313b95ae2c
Diogo Fernandes <diogo_fernandes98@sapo.pt>
2020-05-19 23:51:53 +0100

    teste filesystem

--- source/fdr.c
+++ source/fdr.c
@@ -51,7 +51,7 @@ int flightDataRecorder(void * input){
             writeToRecord(message.mesg_text);
             printf("last message: %s\n", last_mesg);
             printf("messafe.mesg_text %s\n", message.mesg_text);
-            snprintf(last_mesg, message.mesg_text, sizeof(last_mesg));
+            strncpy(last_mesg, message.mesg_text, 1024);
             // display the message 
             printf("[FDR] Dados Recebidos: %s \n", message.mesg_text);
         } 
